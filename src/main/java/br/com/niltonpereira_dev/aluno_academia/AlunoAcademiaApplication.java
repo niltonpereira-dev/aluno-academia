2cd0c5ac909e3b6e82bfeca60fadbe949395291d
Nilton Pereira <niltonpereira.dev@gmail.com>
2026-06-16 19:43:59 -0300

    inicio do projeto

---
+++ src/main/java/br/com/niltonpereira_dev/aluno_academia/AlunoAcademiaApplication.java
@@ -0,0 +1,13 @@
+package br.com.niltonpereira_dev.aluno_academia;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AlunoAcademiaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AlunoAcademiaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/niltonpereira_dev/aluno_academia/AlunoAcademiaApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.niltonpereira_dev.aluno_academia;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AlunoAcademiaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
